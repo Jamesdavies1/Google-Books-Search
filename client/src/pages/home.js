e6016bd17c4e80d3a1623014a08623dcff9187f0
james davies <jamesdavies@jamess-imac.home>
2020-05-01 22:09:17 +0100

    changed input type from email to input

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,30 +1,35 @@
 import React from "react";
 
-
-function Home (){
-
-
-    
-    return(
-        <div>
-        <div className="jumbotron jumbotron-fluid">
-  <div className="container">
-    <h1 className="display-4">google books!</h1>
-    <p className="lead">Find your favourite books using the google book API</p>
-  </div>
-</div>
-<form>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Search book here</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <small id="emailHelp" className="form-text text-muted">Lets find your favourite book!</small>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-
-</div>
-
-    )
+function Home() {
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">google books!</h1>
+          <p className="lead">
+            Find your favourite books using the google book API
+          </p>
+        </div>
+      </div>
+      <form>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Search book here</label>
+          <input
+            type="input"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Lets find your favourite book!
+          </small>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
