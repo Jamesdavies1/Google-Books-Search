456e1b32772437bba40fccb705dc462faf1054f9
james davies <jamesdavies@jamess-imac.home>
2020-05-01 22:09:57 +0100

    split function up into smaller components for readability and reusability

--- controllers/googleAPI.js
+++ controllers/googleAPI.js
@@ -1,32 +1,39 @@
-const axios = require("axios");
 const db = require("../models");
+const makeRequest = require("../utils/makeRequest");
+
+const checkBookIntegrity = books => {
+  return books.filter(
+    book =>
+      book.volumeInfo.title &&
+      book.volumeInfo.infoLink &&
+      book.volumeInfo.authors &&
+      book.volumeInfo.description &&
+      book.volumeInfo.imageLinks &&
+      book.volumeInfo.imageLinks.thumbnail
+  );
+};
+
+const filterDuplicates = (newBooks, existingBooks) =>
+  newBooks.filter(newBook =>
+    existingBooks.every(
+      existingBook => existingBook.googleID.toString() !== newBook.id
+    )
+  );
 
 module.exports = {
   findAll: function(req, res) {
     const { query: params } = req;
-    axios
-      .get("https://www.googleapis.com/books/v1/volumes", {
-        params
-      })
-      .then(results =>
-        results.data.items.filter(
-          result =>
-            result.volumeInfo.title &&
-            result.volumeInfo.infoLink &&
-            result.volumeInfo.authors &&
-            result.volumeInfo.description &&
-            result.volumeInfo.imageLinks &&
-            result.volumeInfo.imageLinks.thumbnail
+
+    makeRequest("https://www.googleapis.com/books/v1/volumes", params)
+      .then(({ data: { items: books } }) => checkBookIntegrity(books))
+      .then(newBooks =>
+        db.Book.find().then(existingBooks =>
+          filterDuplicates(newBooks, existingBooks)
         )
       )
-      .then(apiBooks =>
-        db.Book.find().then(dbBooks =>
-          apiBooks.filter(apiBook =>
-            dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
-          )
-        )
-      )
-      .then(books => res.json(books))
-      .catch(err => res.status(422).json(err));
+      .then(uniqueBooks => res.json(uniqueBooks))
+      .catch(err => {
+        res.status(422).send(err);
+      });
   }
 };
