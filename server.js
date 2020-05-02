69ce8b0f0c21eedbe1a72ea9e39c82bce7f16c7b
james davies <jamesdavies@jamess-imac.home>
2020-05-02 19:25:01 +0100

    attempt to serve build when server starts up

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const cors = require("cors");
 const searchRoute = require("./routes/search");
 const booksRoute = require("./routes/books");
 
+app.use(cors());
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(cors());
+app.use(express.static("client/build"));
 
 // Create the connection to our mongo server
 require("./database")();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("client/build"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
