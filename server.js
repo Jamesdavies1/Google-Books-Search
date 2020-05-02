0e4663642c23ae6eab3ef96ab016c37699a4482f
james davies <jamesdavies@jamess-imac.home>
2020-05-02 18:59:58 +0100

    added app.use to server, telling server to serve client build

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require("./database")();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("client/build"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
